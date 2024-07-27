e7db96de23d2d9c3b2720b32ee2e757178c3a09e
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2024-07-27 22:03:48 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    client_id: "YOUR CLIENT ID",
+    client_secret: "YOUR SECRET",
+    redirect_uri: "http://localhost/callback", //as long as this is the same in your configuration on the spotify API website.
+    scopes: "ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private user-library-modify user-library-read user-read-private user-library-read"
+}
